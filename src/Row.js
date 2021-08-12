427b40f78bf3c133decf49f421375b203d78649c
Kamil_Niemiec <kamil_niemiec@o2.pl>
2021-08-12 16:22:34 +0200

    initial commit, create app and rows

---
+++ src/Row.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import instance from './axios';
+
+const baseUrl = "https://image.tmdb.org/t/p/original/"
+
+
+const Row = ({title, fetchUrl}) => {
+    
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await instance.get(fetchUrl); 
+            setMovies(request.data.results)
+        } 
+        fetchData();
+    }, [fetchUrl])
+
+    return (
+        <div className="row"> 
+            <h2>{title}</h2>
+            <div className="row__posters">
+            {movies.map(movie => <img 
+            className="row__poster" 
+            src={`${baseUrl}${movie.poster_path}`} 
+            alt={movie.name}/>)}
+            </div>
+
+        </div>
+     );
+}
+ 
+export default Row;
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const apiKey = 'ae30d08526847ee944b20fd2f63fcd81';
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${apiKey}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${apiKey}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated/?api_key=${apiKey}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${apiKey}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${apiKey}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${apiKey}&with_genres=99`,
+}
+
+export default requests;
